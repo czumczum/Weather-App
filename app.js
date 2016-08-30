88bdf29e73f70fe22f33a1514ab3fc70a5ad23d9
czumczum <e.mindjunk@gmail.com>
2016-08-30 12:08:42 +0200

    Add some features
    bg image and days switching

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ weatherForecastApp.config(function ($routeProvider) {
             templateUrl: 'pages/second.html',
             controller: 'secondController'
     })
+        .when ('/second/:days', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
 
 });
 
@@ -28,31 +32,31 @@ weatherForecastApp.controller('mainController', ['$scope', 'binderService', func
     })
 }]);
 
-weatherForecastApp.controller('secondController', ['$scope', '$resource', 'binderService', '$routeParams', function ($scope, $resource, binderService, $routeParams) {
+weatherForecastApp.controller('secondController', ['$scope', '$resource', 'binderService', '$routeParams', '$log', function ($scope, $resource, binderService, $routeParams, $log) {
 
+//API configuration
     $scope.city = binderService.city;
+    $scope.days = $routeParams.days || binderService.dayCount;
+
     $scope.weatherAPI =
         $resource('http://api.openweathermap.org/data/2.5/forecast/daily', {
         callback: "JSON_CALLBACK"
     }, {get: {method: "JSONP"}});
-    $scope.days = binderService.dayCount;
 
     $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, appId: '1be079e4d2e906ac71313a709c15b59f' });
 
+//Display functions
     $scope.convertToFahrenheit = function (degK) {
         return Math.round((1.8 * (degK - 273)) + 32);
     };
     $scope.convertToCelsius = function (degK) {
         return Math.round(degK - 272.15);
     };
-
     $scope.convertToDate = function(dt) {
         return new Date(dt * 1000);
-    }
+    };
 
-    console.log($scope.weatherResult);
-
-
+    $log.info($scope.weatherResult);
 }]);
 
 weatherForecastApp.service('binderService', function() {
